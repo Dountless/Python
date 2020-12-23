b4e420bae573f5b1d25b20c1c3996b6761b002d4
Dountless <bestfrd243601@gmail.com>
2020-12-23 23:46:44 +0530

    Dictionary project class

--- Basic Projects/MyDictionay.py
+++ Basic Projects/MyDictionay.py
@@ -4,71 +4,77 @@ from csv import DictReader,DictWriter
 Title="-:WELCOME TO DOUNTLESS DICTIONARY:-"
 print(Title.center(len(Title)+100,"-"))
 
-def Template(i):
-    '''show the menue of dictionary'''
-    print("\n----------------------------------------------------------------------------------------------------------------------------")
-    print("\n [show dictionary_words:->1] [add dictionary:->2]  [delete dictionary:->3] \
-         [search_word:->4] [quit dictionary:->5]")
-    print("----------------------------------------------------------------------------------------------------------------------------\n")
-    try:
-         return int(input("Enter number for choice:->"))
-    except ValueError:
-        print("\n\t\t\t\t-----YOU DON'T TYPE ANY Choice Number------\t\n")
+class Dictionary:
 
-def showDict():
-    '''show the word of dictionary'''
-    try:
+    def Template(self,i):
+        '''show the menue of dictionary'''
+        #show options
+        print("".center(150,"-"))
+        print("\n [show dictionary_words:->1] [add dictionary:->2]  [delete dictionary:->3] \
+            [search_word:->4] [quit dictionary:->5]")
+        print("".center(150,"-"))
+        #check user is correct or not 
+        try:
+            return int(input("Enter number for choice:->"))
+        except ValueError:
+            print("\n\t\t\t\t-----YOU DON'T TYPE ANY Choice Number------\t\n")
 
-        with open('dict.csv','r') as f:
-            #object..iterator
-            show_dict=DictReader(f)
-            #check dict is empty or not
-            if os.stat('dict.csv').st_size==0:
-                x='Dictionary is Empty'
-                print(x.center(len(x)+100,"-"))
-            else:
-                print("Word is :->",end="")
-                for i in show_dict:
-                    print(i['Word'],end=",")
-    except FileNotFoundError:
-        print("\nDictionary is empty .....\n")
+    def showDict(self):
+        '''show the word of dictionary'''
+        try:
 
-        
-def addWord(word):
-    '''add any word in dictionary'''
-    #open file
-    with open('dict.csv','a',newline="") as f:
-        add_word=DictWriter(f,fieldnames=['Word','Meaning'])
-        #check dict is empty or not
-        if os.stat("dict.csv").st_size==0:
-            add_word.writeheader()
-            #add word to dictionary
-        if word=="":
-            print("\n\t\t\t\t-----YOU DON'T TYPE ANY WORD------\t\n")
-        add_word.writerow({
-                'Word':word,
-                'Meaning':input(f"Enter meaning of {word}:->").lower()
-                })
-        print("\n\t\t\t\tAdded Successfully...")
-        
+            with open('dict.csv','r') as f:
+                #object..iterator
+                show_dict=DictReader(f)
+                #check dict is empty or not
+                if os.stat('dict.csv').st_size==0:
+                    # styl to print message
+                    x='Dictionary is Empty'
+                    print(x.center(len(x)+100,"-"))
+                else:
+                    print("Word is :->",end="")
+                    for i in show_dict:
+                        print(i['Word'],end=",")
+        except FileNotFoundError:
+            print("\nDictionary is empty .....\n")
+        return 1
+
             
-def deletedictionary():
-    '''delete whole word ,meaning in dictionary'''
-    #open dictionary
-    with open("dict.csv",'w') as f:
-        pass
+    def addWord(self,word):
+        '''add any word in dictionary'''
+        #open file
+        with open('dict.csv','a',newline="") as f:
+            add_word=DictWriter(f,fieldnames=['Word','Meaning'])
+            #check dict is empty or not
+            if os.stat("dict.csv").st_size==0:
+                add_word.writeheader()
+                #add word to dictionary
+            if word=="":
+                print("\n\t\t\t\t-----YOU DON'T TYPE ANY WORD------\t\n")
+            add_word.writerow({
+                    'Word':word,
+                    'Meaning':input(f"Enter meaning of {word}:->").lower()
+                    })
+            print("\n\t\t\t\tAdded Successfully...")
+            
+                
+    def deletedictionary(self):
+        '''delete whole word ,meaning in dictionary'''
+        #open dictionary
+        with open("dict.csv",'w') as f:
+            pass
 
-def searchWord(word):
-    '''search word and give meaning of that word'''
-    #open dictionary
-    with open("dict.csv") as f:
-        mix_dict=DictReader(f)
-        for i in mix_dict:
-            if word in i.values():
-                print(f"Meaning is:-> {i.get('Meaning')}\n")
-                break
-        else:
-            print("\n\t\tNOT FOUND !")
+    def searchWord(self,word):
+        '''search word and give meaning of that word'''
+        #open dictionary
+        with open("dict.csv") as f:
+            mix_dict=DictReader(f)
+            for i in mix_dict:
+                if word in i.values():
+                    print(f"Meaning is:-> {i.get('Meaning')}\n")
+                    break
+            else:
+                print("\n\t\tNOT FOUND !")
     
 
 
@@ -78,19 +84,20 @@ if __name__=="__main__":
 
         while True:
             #user choice number
-            ch=Template(input("\n\n\t\t\t\t\t\t\t\t\t\tpress Enter...\n\n\n\n"))
+            Dict=Dictionary()
+            ch=Dict.Template(input("\n\n\t\t\t\t\t\t\t\t\t\tpress Enter...\n\n\n\n"))
 
             if ch==1:
-                showDict()
+                Dict.showDict()
             elif ch==2:
-                addWord(input("Enter word:"))
+                Dict.addWord(input("Enter word:"))
             elif ch==3:
-                deletedictionary()
+                Dict.deletedictionary()
                 print("\n\t\t\tDelete Successfully......")
             elif ch==4:
-                if showDict()==None:
+                if Dict.showDict()==None:
                     continue
-                searchWord(input("\nEnter word:"))
+                Dict.searchWord(input("\nEnter word:"))
             elif ch==5:
                 sys.exit()
             else:
